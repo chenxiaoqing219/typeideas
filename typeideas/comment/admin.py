c9eb2154d7a2f08b28763c3c35df5339a34232f4
chenxiaoqing219 <493161152@qq.com>
2020-03-26 22:05:22 +0800

    '3月26日代码提交'

--- typeideas/comment/admin.py
+++ typeideas/comment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from typeideas.custom_site import custom_site
+from .models import Comment
+# from typeideas.base_admin import BaseOwnerAdmin
+
+@admin.register(Comment, site=custom_site)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('target', 'nickname', 'content', 'website', 'created_time')
